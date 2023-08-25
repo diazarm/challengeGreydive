b083ff5f7343dc238f1471c5a188e881f210b0f8
diazarm <marceloardiaz@gmail.com>
2023-08-24 22:27:50 -0400

    update front

--- client/greydive/src/views/Home/Home.jsx
+++ client/greydive/src/views/Home/Home.jsx
@@ -1,71 +1,134 @@
-//import stylesHome from "./Home.module.css";
 import { useState } from 'react';
 import axios from 'axios';
-import './App.css';
+import "../Home/styles.css";
 
 function Home() {
-    
-    const [surveyData, setSurveyData] = useState(null);
-    const [submitted, setSubmitted] = useState(null);
+  const [surveyData, setSurveyData] = useState(null);
 
-    const fetchSurveyData = async () => {
-        try {
-          const response = await axios.get("http://localhost:3001/responses"); 
-          console.log(response.data)
-          setSurveyData(response.data);
-        } catch (error) {
-          console.error('Error fetching survey data:', error);
-        }
-      };
+  const [formData, setFormData] = useState({
+    full_name: '',
+    email: '',
+    birth_date: '',
+    country_of_origin: '',
+    terms_and_conditions: false,
+  });
 
-      const handlerClean = () =>{setSurveyData(null)};
-      
-      const handleSubmit = async () => {
-        try {
-          await axios.post('http://localhost:3001/responses', selectedOptions);
-          setSubmitted(true);
-        } catch (error) {
-          console.error('Error submitting survey:', error);
-        }
-      };
-      const createSurvey = () => {
-        handleSubmit()  
-      }
-  return(
-<div>
-    <div>
+  const handleInputChange = (event) => {
+    const { name, value, type, checked } = event.target;
+
+    // Si es un checkbox, manejamos el valor "checked" en lugar de "value"
+    const inputValue = type === 'checkbox' ? checked : value;
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: inputValue,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axios.post('http://localhost:3001/responses', formData);
+      console.log('Form submitted successfully');
+      // Actualizar la lista después del envío
+      fetchSurveyData();
+    } catch (error) {
+      console.error('Error submitting survey:', error);
+    }
+  };
+
+  const fetchSurveyData = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/responses');
+      setSurveyData(response.data);
+    } catch (error) {
+      console.error('Error fetching survey data:', error);
+    }
+  };
+
+  const handleClean = () => {
+    setSurveyData(null);
+  };
+
+  return (
+    <div className="container">
+      <div>
         <h1>Survey app</h1>
-    </div>
-    <div>
-            <button onClick={createSurvey}>Create survey</button>
-    </div>
-    {surveyData ? (
-   
-    <div>
+      </div>
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            name="full_name"
+            placeholder="Full Name"
+            value={formData.full_name}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={formData.email}
+            onChange={handleInputChange}
+          />
+          <input
+            type="text"
+            name="birth_date"
+            placeholder="Birth Date"
+            value={formData.birth_date}
+            onChange={handleInputChange}
+          />
+          <select
+            name="country_of_origin"
+            value={formData.country_of_origin}
+            onChange={handleInputChange}
+          >
+            <option value="">Select a country</option>
+            <option value="argentina">Argentina</option>
+            <option value="brasil">Brasil</option>
+            <option value="chile">Chile</option>
+            <option value="colombia">Colombia</option>
+            <option value="mexico">México</option>
+            <option value="peru">Perú</option>
+            <option value="uruguay">Uruguay</option>
+            <option value="venezuela">Venezuela</option>
+          </select>
+          <input
+            type="checkbox"
+            name="terms_and_conditions"
+            checked={formData.terms_and_conditions}
+            onChange={handleInputChange}
+          />
+          <label htmlFor="terms_and_conditions">
+            I accept the terms and conditions
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        <button onClick={handleClean}>Clear List</button>
+      </div>
+      {surveyData ? (
+          <div>
           <h2>Survey Responses</h2>
+          <div className="responses">
           {surveyData.map((response) => (
-            <div key={response.id}>
+            <div key={response.id} className="response">
               <h3>Full Name: {response.full_name}</h3>
               <h3>Id number: {response.id}</h3>
               <p>Email: {response.email}</p>
-              <p>Birth Date: {response.birth_date}</p>
               <p>Country of Origin: {response.country_of_origin}</p>
               <p>Terms and Conditions: {response.terms_and_conditions ? 'Accepted' : 'Not Accepted'}</p>
             </div>
           ))}
-            <button onClick={handlerClean}>clear list</button>
-
-    </div>) : (
-         <div>
-            <button onClick={fetchSurveyData}>Load Survey</button>
+          </div>
         </div>
-    )}
-</div>
-  )
-  
+      ) : (
+        <div>
+          <button onClick={fetchSurveyData}>Load Survey</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
-
 export default Home;
-
 
